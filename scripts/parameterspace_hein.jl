34c0f873e8f24ae5225b8bece08347cb7fb9b8bf
Riccardo Foffi <riccardo.foffi@protonmail.com>
2023-04-16 17:42:06 +0200

    fix parameters and evaluate phycosphere radius (hein)

--- scripts/parameterspace_hein.jl
+++ scripts/parameterspace_hein.jl
@@ -6,8 +6,8 @@ function parameterspace_hein()
     # generate 100 log-uniform radii between 0.1 and 100 μm
     R = exp10.(range(-1, 2, length=100))u"μm"
     # use phytoplankton leakage rates to set bound values
-    L₁ = leakage_rate(R[1], 0.01)
-    L₂ = leakage_rate(R[end], 0.1)
+    L₁ = leakage_rate(R[1], 0.01) |> u"pmol/s"
+    L₂ = leakage_rate(R[end], 0.1) |> u"pmol/s"
     # generate 100 log-uniform values between L₁ and L₂
     L = exp10.(range(log10(ustrip(L₁)), log10(ustrip(L₂)), length=100))u"pmol/s"
     @save datadir("Hein", "paramspaceRL.jld2") R L
---
+++ scripts/phycosphere_hein.jl
@@ -0,0 +1,33 @@
+using DrWatson
+@quickactivate :GradientSensing
+using JLD2
+
+function phycosphere_hein()
+    f = jldopen(datadir("Hein", "paramspaceRL.jld2"))
+    R, L = f["R"], f["L"]
+    
+    # set C₀, T, U, Π to some default values
+    # can't assign units now or they are ignored in the output filename
+    C₀ = 1#u"nM"
+    T = 100#u"ms"
+    U = 46.5#u"μm/s"
+    Π = 6
+    params = @strdict C₀ T U Π
+    
+    produce_or_load(
+        datadir("Hein"), params;
+        prefix="HeinRadius", suffix="jld2",
+        tag = false
+    ) do params
+        # reassign units to parameters
+        C₀ = params["C₀"]u"nM"
+        T = params["T"]u"ms"
+        U = params["U"]u"μm/s"
+        Π = params["Π"]u"1"
+        # evaluate phycosphere radius for each (R,L) pair
+        H = map(p -> HeinRadius(p..., C₀, T, U, Π), Iterators.product(R,L))
+        @strdict H
+    end
+end
+
+phycosphere_hein()

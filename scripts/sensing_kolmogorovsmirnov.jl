e97bda345d176305dc1d56d12e5de492e984b480
Riccardo Foffi <riccardo.foffi@protonmail.com>
2023-06-21 15:28:06 +0200

    change paths and rewrite for distributed computing

--- scripts/sensing_kolmogorovsmirnov.jl
+++ scripts/sensing_kolmogorovsmirnov.jl
@@ -1,75 +1,15 @@
-using DrWatson
-@quickactivate :GradientSensing
-using JLD2, DataFrames, Distributions, HypothesisTests
-
-function sensing_kolmogorovsmirnov()
-    f = jldopen(datadir("Poisson", "RC.jld2"))
-    R, Cₛ = f["R"], f["Cₛ"]
-    sensing_from_waitingtimes(R, Cₛ)
+using Distributed
+@everywhere using DrWatson
+@everywhere @quickactivate :GradientSensing
+@everywhere begin
+    using JLD2, DataFrames, Distributions, HypothesisTests
 end
 
-function sensing_from_waitingtimes(R::AbstractVector, Cₛ::AbstractVector)
-    for i in eachindex(R)
-        datasets = get_datasets_waitingtimes(R[i])
-        foreach(sensing_from_waitingtimes, eachrow(datasets))
-    end
-end
-
-"""
-    get_datasets_waitingtimes(R)
-Collects all the waiting time datasets for source radius `R`.
-"""
-function get_datasets_waitingtimes(R)
-    Rsig3 = round(typeof(1.0u"μm"), R, sigdigits=3) |> ustrip
-    collect_results(
-        datadir("Poisson");
-        rinclude = [Regex("waitingtimes.*R=$(Rsig3)")]
-    )
-end
-
-"""
-    sensing_from_waitingtimes(df)
-Performs a Kolmogorov-Smirnov sensing test over the data stored in
-the dataframe `df`. See `kstest_transect`.
-Outputs (transect midpoints, KS sensing statistics, fraction of KS successes)
-are saved to file (with suffix "sensing").
-"""
-function sensing_from_waitingtimes(df)
-    params = parse_savename(df.path)[2]
-    produce_or_load(
-        datadir("Poisson", "KolmogorovSmirnov"), params;
-        prefix="sensing", suffix="jld2", tag=false, loadfile=false
-    ) do params
-        r = df.r
-        waitingtimes = df.waitingtimes
-        R = params["R"]u"μm"
-        Cₛ = params["Cₛ"]u"μM"
-        Δt = params["Δt"]u"ms"
-        ks = kstest_transect(waitingtimes, r, Δt, R, Cₛ, C₀)
-        # evaluate mean in each interval
-        ksavg = mean(ks)
-        @strdict r ks ksavg
-    end
-end
 
 # aliases for brevity
-VVQ = AbstractVector{<:AbstractVector{<:Quantity}}
-VVVQ = AbstractVector{<:AbstractVector{<:AbstractVector{<:Quantity}}}
-function kstest_transect(waitingtimes::VVVQ, r, Δt, R, Cₛ, C₀)
-    [kstest_transect(w,r,Δt,R,Cₛ,C₀) for w in waitingtimes]
-end
-"""
-    kstest_transect(waitingtimes, r, Δt, R, Cₛ, C₀)
-Performs a Kolmogorov-Smirnov test (see `kstestright`) for the `waitingtimes`
-at each point in the transect described by `r`.
-`Δt` is the temporal resolution which the sensor has sampled the absorption events.
-`R`, `Cₛ` and `C₀` are used to evaluate the real concentration field at the midpoint
-of each interval in the transect, which defines the reference distribution for
-the absorption waiting times in the KS test.
-"""
-function kstest_transect(waitingtimes::VVQ, r, Δt, R, Cₛ, C₀)
-    c = @. C(r, R, Cₛ, C₀)
-    [kstestright(waitingtimes[i], c[i], Δt) for i in eachindex(c)]
+@everywhere begin
+    VVQ = AbstractVector{<:AbstractVector{<:Quantity}}
+    VVVQ = AbstractVector{<:AbstractVector{<:AbstractVector{<:Quantity}}}
 end
 
 """
@@ -94,7 +34,8 @@ If this condition is satisfied, the function returns `1.0`
 (i.e. the sensor has observed a statistically significant decrease in waiting times),
 otherwise (p≥0.05) the function returns `0.0`.
 """
-function kstestright(waitingtimes, c, Δt)
+
+@everywhere function kstestright(waitingtimes, c, Δt)
     isempty(waitingtimes) && return Float64(false)
     # need to convert all values to same units and then ustrip
     # because HypothesisTests functions don't work with units
@@ -108,4 +49,86 @@ function kstestright(waitingtimes, c, Δt)
     return Float64(pvalue(KStest, tail=:right) < 0.05)
 end
 
-sensing_kolmogorovsmirnov()
+
+
+"""
+    kstest_transect(waitingtimes, r, Δt, R, Cₛ, C₀)
+Performs a Kolmogorov-Smirnov test (see `kstestright`) for the `waitingtimes`
+at each point in the transect described by `r`.
+`Δt` is the temporal resolution which the sensor has sampled the absorption events.
+`R`, `Cₛ` and `C₀` are used to evaluate the real concentration field at the midpoint
+of each interval in the transect, which defines the reference distribution for
+the absorption waiting times in the KS test.
+"""
+
+@everywhere function kstest_transect(waitingtimes::VVQ, r, Δt, R, Cₛ, C₀)
+    c = @. C(r, R, Cₛ, C₀)
+    [kstestright(waitingtimes[i], c[i], Δt) for i in eachindex(waitingtimes)]
+end
+@everywhere function kstest_transect(waitingtimes::VVVQ, r, Δt, R, Cₛ, C₀)
+    [kstest_transect(waitingtimes[i],r[i],Δt,R,Cₛ,C₀) for i in eachindex(waitingtimes)]
+end
+
+
+"""
+    get_datasets_waitingtimes(R)
+Collects all the waiting time datasets for source radius `R`.
+"""
+function get_datasets_waitingtimes(R)
+    Rsig3 = round(typeof(1.0u"μm"), R, sigdigits=3) |> ustrip
+    collect_results(
+        datadir("newPoisson");
+        rinclude = [Regex("waitingtimes.*R=$(Rsig3)")]
+    )
+end
+
+"""
+    sensing_from_waitingtimes(df)
+Performs a Kolmogorov-Smirnov sensing test over the data stored in
+the dataframe `df`. See `kstest_transect`.
+Outputs (transect midpoints, KS sensing statistics, fraction of KS successes)
+are saved to file (with suffix "sensing").
+"""
+
+@everywhere function sensing_from_waitingtimes(df)
+    params = parse_savename(df.path)[2]
+    produce_or_load(
+        datadir("newPoisson", "KolmogorovSmirnov"), params;
+        prefix="sensing", suffix="jld2", tag=false, loadfile=false
+    ) do params
+        r = df.r
+        waitingtimes = df.waitingtimes
+        R = params["R"]u"μm"
+        Cₛ = params["Cₛ"]u"μM"
+        Δt = params["Δt"]u"ms"
+        ks = kstest_transect(waitingtimes, r, Δt, R, Cₛ, C₀)
+        waitingtimes = nothing
+        GC.gc()
+        # evaluate mean in each interval
+        ksavg = mean(ks)
+        @strdict r ks ksavg
+    end
+    GC.gc()
+    return nothing
+end
+
+"""
+    produce_data(R::AbstractVector, Cₛ::AbstractVector)
+Run sensing analysis on each dataset.
+Collects datasets in chunks by values of R to avoid overloading memory.
+"""
+function produce_data(R::AbstractVector, Cₛ::AbstractVector)
+    for i in eachindex(R)
+        datasets = get_datasets_waitingtimes(R[i])
+        GC.gc()
+        pmap(sensing_from_waitingtimes, eachrow(datasets))
+        datasets = nothing
+        GC.gc()
+    end
+end
+
+##
+f = jldopen(datadir("newPoisson", "RC.jld2"))
+R, Cₛ = f["R"], f["Cₛ"]
+R = R[1:end-1]
+produce_data(R, Cₛ)

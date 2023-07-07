e80c59691f8ef61861a1a19c63ca2dc00a03d8d2
Riccardo Foffi <riccardo.foffi@protonmail.com>
2023-07-07 15:16:05 +0200

    use makie for plots

--- scripts/plots/ic_kolmogorovsmirnov.jl
+++ scripts/plots/ic_kolmogorovsmirnov.jl
@@ -1,33 +1,134 @@
+##
 using DrWatson
 @quickactivate :GradientSensing
 using JLD2, DataFrames
-using Plots; Plots.pyplot()
-Plots.default(
-    thickness_scaling=1.5, frame=:border, grid=false,
-    bgcolorlegend=:transparent, fgcolorlegend=:transparent,
-    palette=:Dark2
+using CairoMakie
+using PublicationFiguresMakie
+set_theme!(Publication,
+    Axis=(
+        xticksvisible=false, yticksvisible=false,
+        xminorticksvisible=false, yminorticksvisible=false,
+    )
 )
 
-function plot_ic_ks()
-    datasets = collect_results(
-        datadir("newPoisson","KolmogorovSmirnov"),
-        rinclude=[r"IC"]
+
+##
+function makeplot(datasets, R, Cₛ)
+    clims = (0, 4.5)
+    clevels = range(clims..., step=0.25)
+    cmap = :viridis
+
+    fig = Figure(resolution = TwoColumns(1));
+    Colorbar(fig[1,4],
+        colormap = cmap,
+        colorrange = clims,
+        ticks = 0:4,
+        label = "log(IC)"
     )
-    for df in eachrow(datasets)
-        f = jldopen(datadir("newPoisson", "RC.jld2"))
-        R, Cₛ = f["R"], f["Cₛ"]
-        R = R[1:end-1]
+    labels = ["A", "B", "C"]
+
+    for (i,df) in enumerate(eachrow(datasets))
         ic = df.ic
-
-        heatmap(R, Cₛ, log10.(ic)', scale=:log10)
-        plot!(xlab="R (μm)", ylab="Cₛ (μM)", cbartitle="log(IC)")
-
-        # parse dataset parameters
-        params = parse_savename(df.path)[2]
-        # save both svg and png
-        savefig(plotsdir("newPoisson", "KolmogorovSmirnov", savename("IC", params, "svg")))
-        savefig(plotsdir("newPoisson", "KolmogorovSmirnov", savename("IC", params, "png")))
+        ax, _ = heatmap(fig[1,i], R, Cₛ, log10.(ic),
+            colormap = cmap,
+            colorrange = clims,
+        )
+        tightlimits!(ax)
+        ax.xscale = log10
+        ax.yscale = log10
+        ax.xlabel = "R (μm)"
+        if i == 1
+            ax.ylabel = "Cₛ (μM)"
+        end
+        Label(fig[1, i, TopLeft()], labels[i], halign=:right)
     end
+    fig
 end
 
-plot_ic_ks()
+##
+#==
+function plot_phycosphere_hein_byDc()
+    f = jldopen(datadir("Poisson", "RC.jld2"))
+    R, Cₛ = ustrip.(f["R"]), ustrip.(f["Cₛ"])
+    close(f)
+
+    alldatasets = unpack_dataframe(
+        collect_results(datadir("Poisson", "KolmogorovSmirnov"), rinclude=[r"IC"])
+    )
+    Ū = 50
+    T̄ = 100
+    datasets = subset(alldatasets,
+        :U => U -> U .== Ū,
+        :T => T -> T .== T̄,
+    )
+    sort!(datasets, [:Dc])
+    
+    fig = makeplot(datasets, R, Cₛ);
+    for (i,df) in enumerate(eachrow(datasets))
+        ax = contents(fig[1,i])[1]
+        ax.title = "Dc = $(df.Dc)μm²/s"
+    end
+
+    save(plotsdir("Poisson", "KolmogorovSmirnov", "IC_varDc_T=$(T̄)_U=$(Ū).svg"), fig)
+end
+
+plot_phycosphere_hein_byDc()
+==#
+
+##
+#==
+function plot_phycosphere_hein_byU()
+    f = jldopen(datadir("Poisson", "RC.jld2"))
+    R, Cₛ = ustrip.(f["R"]), ustrip.(f["Cₛ"])
+    close(f)
+
+    alldatasets = unpack_dataframe(
+        collect_results(datadir("Poisson", "KolmogorovSmirnov"), rinclude=[r"IC"])
+    )
+    D̄ = 500
+    T̄ = 100
+    datasets = subset(alldatasets,
+        :Dc => D -> D .== D̄,
+        :T => T -> T .== T̄,
+    )
+    sort!(datasets, [:U])
+    
+    fig = makeplot(datasets, R, Cₛ)
+    for (i,df) in enumerate(eachrow(datasets))
+        ax = contents(fig[1,i])[1]
+        ax.title = "U = $(df.U)μm/s"
+    end
+
+    save(plotsdir("Poisson", "KolmogorovSmirnov", "IC_varU_Dc=$(D̄)_T=$(T̄).svg"), fig)
+end
+
+plot_phycosphere_hein_byU()
+==#
+
+##
+function plot_phycosphere_hein_byT()
+    f = jldopen(datadir("Poisson", "RC.jld2"))
+    R, Cₛ = ustrip.(f["R"]), ustrip.(f["Cₛ"])
+    close(f)
+
+    alldatasets = unpack_dataframe(
+        collect_results(datadir("Poisson", "KolmogorovSmirnov"), rinclude=[r"IC"])
+    )
+    D̄ = 500
+    Ū = 50
+    datasets = subset(alldatasets,
+        :Dc => D -> D .== D̄,
+        :U => U -> U .== Ū,
+    )
+    sort!(datasets, [:T])
+    
+    fig = makeplot(datasets, R, Cₛ);
+    for (i,df) in enumerate(eachrow(datasets))
+        ax = contents(fig[1,i])[1]
+        ax.title = "T = $(df.T)ms"
+    end
+
+    save(plotsdir("Poisson", "KolmogorovSmirnov", "IC_varT_Dc=$(D̄)_U=$(Ū).svg"), fig)
+end
+
+plot_phycosphere_hein_byT()

23cb076551783cf195c9f304d06ccf5f23d2aded
Riccardo Foffi <riccardo.foffi@protonmail.com>
2023-07-14 14:28:34 +0200

    add IC vs C plot

--- scripts/plots/reward_landscape.jl
+++ scripts/plots/reward_landscape.jl
@@ -33,10 +33,11 @@ f = jldopen(datadir("HeinMod", "RC.jld2"), "r")
 R = ustrip.(f["R"])
 Cₛ = ustrip.(f["Cₛ"])
 close(f)
+
+##
 R̄ = [0.5, 1.0, 2.5, 5.0, 10.0, 25.0, 50.0]
 js = map(r -> findfirst(R .> r), R̄)
 
-##
 R̄ = R[js]
 Ī = ic[js,:]
 cmap = palette(:thermal, length(R̄))
@@ -50,4 +51,23 @@ ax.xlabel = "Cₛ (μM)"
 ax.ylabel = "IC"
 xlims!(ax, 3e-3, Cₛ[end])
 leg = axislegend("R (μm)"; position=:lt)
-save(plotsdir("HeinMod", "reward-vs-C.svg"), fig)
+#save(plotsdir("HeinMod", "reward-vs-C.svg"), fig)
+
+##
+C̄ = [0.01, 0.015, 0.02, 0.04, 0.1]
+js = map(c -> findfirst(Cₛ .> c), C̄)
+
+C̄ = Cₛ[js]
+Ī = ic[:,js]'
+cmap = palette(:thermal, length(C̄))
+fig = Figure(resolution = SinglePlot());
+labels = string.(round.(C̄ .* 1e3, sigdigits=2))
+ax, _ = series(fig[1,1], R, Ī, color=cmap, linewidth=9, labels=labels)
+tightlimits!(ax)
+ax.yscale = log10
+ax.xscale = log10
+ax.xlabel = "R (μm)"
+ax.ylabel = "IC"
+xlims!(ax, 0.5, 50)
+leg = axislegend("Cₛ (nM)"; position=:rt)
+#save(plotsdir("HeinMod", "reward-vs-R.svg"), fig)

96c1c78daa37eb7c00e3f7424492c3b7c2a81149
Riccardo Foffi <riccardo.foffi@protonmail.com>
2023-10-09 15:10:14 +0200

    add try-catch for KS phycosphere evaluation

--- scripts/phycosphere_kolmogorovsmirnov.jl
+++ scripts/phycosphere_kolmogorovsmirnov.jl
@@ -41,16 +41,21 @@ function phycosphere_kolmogorovsmirnov(df)
     close(f)
     iter = Iterators.product(R, Cₛ) |> collect
     S = zeros(typeof(1.0u"μm"), size(iter))
+    println(size(S))
     for i in eachindex(iter)
-        R, Cₛ = iter[i]
-        row = subset(df,
-            :R => r -> r .== round(ustrip(R), sigdigits=3),
-            :Cₛ => c -> c .== round(ustrip(Cₛ), sigdigits=3)
-        ) |> first
-        l = length(row.ksavg)
-        r = [collect(s[end-l+1:end]) for s in row.r]
-        mr = mean(r)
-        S[i] = sensing_threshold(mr, row.ksavg, R)
+        try
+            R, Cₛ = iter[i]
+            row = subset(df,
+                :R => r -> r .== round(ustrip(R), sigdigits=3),
+                :Cₛ => c -> c .== round(ustrip(Cₛ), sigdigits=3)
+            ) |> first
+            l = length(row.ksavg)
+            r = [collect(s[end-l+1:end]) for s in row.r]
+            mr = mean(r)
+            S[i] = sensing_threshold(mr, row.ksavg, R)
+        catch
+            S[i] = NaN * 1u"μm"
+        end
     end
     S
 end
@@ -59,15 +64,15 @@ end
     sensing_threshold(transect, sensing, R)
 Finds the first position along the transect where sensing
 (as defined by the average of the one-tailed KS test over independent samples)
-occurs with a probability greater than 0.68.
+occurs with a probability greater than 0.99.
 
 The transect is in reverse order, i.e. it starts away from the source and
-moves towards it. If there is no position at which `sensing > 0.68`,
+moves towards it. If there is no position at which `sensing > 0.99`,
 then the function returns the radius of the source (`R`) itself,
 meaning that there is no distance at which sensing can occur consistently.
 """
 function sensing_threshold(grid, sensing, R)
-    idx = findfirst(sensing .> 0.68)
+    idx = findfirst(sensing .> 0.99)
     isnothing(idx) ? R : grid[idx]
 end
 

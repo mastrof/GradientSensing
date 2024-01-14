79eb7c029369eed66e1401e0733adf577c6c8997
Riccardo Foffi <riccardo.foffi@protonmail.com>
2024-01-14 22:07:12 +0100

    add finite difference approach (temporary, needs further work)

--- src/phycosphere_Hein.jl
+++ src/phycosphere_Hein.jl
@@ -1,4 +1,4 @@
-export HeinRadius, HeinModRadius
+export HeinRadius, HeinModRadius, HeinFDRadius
 
 """
     HeinRadius(R, L, C₀, T, Dc, U, Π; q=1)
@@ -15,17 +15,17 @@ Find the distance from the source at which the SNR drops below the threshold.
 - `q`: SNR threshold, defaults to 1
 """
 function HeinRadius(
-    R::Quantity{<:Real, 𝐋},
-    L::Quantity{<:Real, 𝐍/𝐓},
-    C₀::Quantity{<:Real, 𝐍/𝐋^3},
-    T::Quantity{<:Real, 𝐓},
-    Dc::Quantity{<:Real, 𝐋^2/𝐓},
-    U::Quantity{<:Real, 𝐋/𝐓},
+    R::Quantity{<:Real,𝐋},
+    L::Quantity{<:Real,𝐍 / 𝐓},
+    C₀::Quantity{<:Real,𝐍 / 𝐋^3},
+    T::Quantity{<:Real,𝐓},
+    Dc::Quantity{<:Real,𝐋^2 / 𝐓},
+    U::Quantity{<:Real,𝐋 / 𝐓},
     Π;
     q=1
 )
     # equation to solve: SNR = q
-    f(r) = snr(r,L,C₀,T,Dc,U,Π) - q
+    f(r) = snr(r, L, C₀, T, Dc, U, Π) - q
     # try to solve SNR=q
     # if a solution cannot be found, there is no phycosphere
     # so h is set to h=R
@@ -53,17 +53,17 @@ Find the distance from the source at which the SNR drops below the threshold.
 - `q`: SNR threshold, defaults to 1
 """
 function HeinRadius(
-    R::Quantity{<:Real, 𝐋},
-    Cₛ::Quantity{<:Real,𝐍/𝐋^3},
-    C₀::Quantity{<:Real, 𝐍/𝐋^3},
-    T::Quantity{<:Real, 𝐓},
-    Dc::Quantity{<:Real, 𝐋^2/𝐓},
-    U::Quantity{<:Real, 𝐋/𝐓},
+    R::Quantity{<:Real,𝐋},
+    Cₛ::Quantity{<:Real,𝐍 / 𝐋^3},
+    C₀::Quantity{<:Real,𝐍 / 𝐋^3},
+    T::Quantity{<:Real,𝐓},
+    Dc::Quantity{<:Real,𝐋^2 / 𝐓},
+    U::Quantity{<:Real,𝐋 / 𝐓},
     Π;
     q=1
 )
-# equation to solve: SNR = q
-    f(r) = snr(r,R,Cₛ,C₀,T,Dc,U,Π) - q
+    # equation to solve: SNR = q
+    f(r) = snr(r, R, Cₛ, C₀, T, Dc, U, Π) - q
     # try to solve SNR=q
     # if a solution cannot be found, there is no phycosphere
     # so h is set to h=R
@@ -80,9 +80,10 @@ end
     HeinModRadius(R, Cₛ, C₀, T, Dc, U, Π; q=1)
 Find the distance from the source at which the modulated SNR drops below the threshold.
 """
-function HeinModRadius(R, Cₛ::Quantity{<:Real,𝐍/𝐋^3}, C₀, T, Dc, U, Π; q=1)
-    λ = U*T
-    f(r) = snr(r,R,Cₛ,C₀,T,Dc,U,Π)*modulator((r/λ)^1.5) - q
+function HeinModRadius(R, Cₛ::Quantity{<:Real,𝐍 / 𝐋^3}, C₀, T, Dc, U, Π; q=1)
+    λ = U * T
+    f(r) = snr(r, R, Cₛ, C₀, T, Dc, U, Π) * modulator((r / λ)^1.5) - q
+    #f(r) = snr(r, R, Cₛ, C₀, T, Dc, U, Π) * fp(r / λ) - q
     h = try
         find_zero(f, R)
     catch e
@@ -92,3 +93,22 @@ function HeinModRadius(R, Cₛ::Quantity{<:Real,𝐍/𝐋^3}, C₀, T, Dc, U, Π
 end
 
 @inline modulator(x) = 1 - exp(-x)
+@inline fp(x) = min((x / 2)^1, 1.0)
+
+"""
+    HeinFDRadius(R, Cₛ, C₀, T, Dc, U, Π; q=1)
+Find the distance from the source at which the SNR, with the signal estimated
+via finite differences, drops below the threshold.
+"""
+function HeinFDRadius(R, Cₛ::Quantity{<:Real,𝐍 / 𝐋^3}, C₀, T, Dc, U, Π; q=1)
+    λ = U * T
+    f(r) = snrFD(r, R, Cₛ, C₀, T, Dc, U, Π) - q
+    #f(r) = snr(r, R, Cₛ, C₀, T, Dc, U, Π) * (1 / (1 + λ / r)) - q
+    h = try
+        find_zero(f, 2R, Order16(); xatol=1e-16u"μm")
+    catch e
+        #println(e)
+        R
+    end
+    h > R ? h : R
+end
--- src/sensing.jl
+++ src/sensing.jl
@@ -14,7 +14,7 @@ Returns the result in "μM/s".
 """
 @inline function σMW(r, L, C₀, T, Dc)
     Cr = C(r, L, C₀, Dc)
-    sqrt(3*Cr / (π*a*Dc*T^3*Unitful.Na)) |> u"μM/s"
+    sqrt(3 * Cr / (π * a * Dc * T^3 * Unitful.Na)) |> u"μM/s"
 end
 """
     σMW(r, R, Cₛ, C₀, T)
@@ -32,7 +32,7 @@ Returns the result in "μM/s".
 """
 @inline function σMW(r, R, Cₛ, C₀, T, Dc)
     Cr = C(r, R, Cₛ, C₀)
-    sqrt(3*Cr / (π*a*Dc*T^3*Unitful.Na)) |> u"μM/s"
+    sqrt(3 * Cr / (π * a * Dc * T^3 * Unitful.Na)) |> u"μM/s"
 end
 
 """
@@ -40,13 +40,22 @@ end
 True signal from a temporal measurement of a spatial gradient
 (equivalent to a convective derivative).
 """
-@inline signal(r, L, U, Dc) = U*∇C(r,L,Dc) |> u"μM/s"
+@inline signal(r, L, U, Dc) = U * ∇C(r, L, Dc) |> u"μM/s"
 """
     signal(r, R, Cₛ, U)
 True signal from a temporal measurement of a spatial gradient
 (equivalent to a convective derivative).
 """
-@inline signal(r, R, Cₛ, U) = U*∇C(r,R,Cₛ) |> u"μM/s"
+@inline signal(r, R, Cₛ, U) = U * ∇C(r, R, Cₛ) |> u"μM/s"
+
+finitediff_b(f, x, δ) = (f(x) - f(x + δ)) / δ # backward
+#finitediff_b(f, x, δ) = (+3f(x) - 4f(x + δ / 2) + f(x + δ)) / δ # backward 2nd order
+#finitediff_b(f, x, δ) = (f(x - δ / 2) - f(x + δ / 2)) / δ # central
+@inline function signalFD(r, R, Cₛ, C₀, U, T)
+    f(r) = C(r, R, Cₛ, C₀)
+    dC = finitediff_b(f, r, U * T)
+    U * dC |> u"μM/s"
+end
 
 """
     noise(r, L, C₀, T, Dc, Π)
@@ -74,7 +83,7 @@ Signal to noise ratio.
 - `U`: convective speed of the sensor
 - `Π`: chemotactic precision factor
 """
-@inline snr(r, L, C₀, T, Dc, U, Π) = signal(r,L,U,Dc)/noise(r,L,C₀,T,Dc,Π)
+@inline snr(r, L, C₀, T, Dc, U, Π) = signal(r, L, U, Dc) / noise(r, L, C₀, T, Dc, Π)
 """
     snr(r, R, Cₛ, C₀, T, Dc, U, Π)
 Signal to noise ratio.
@@ -89,4 +98,6 @@ Signal to noise ratio.
 - `U`: convective speed of the sensor
 - `Π`: chemotactic precision factor
 """
-@inline snr(r, R, Cₛ, C₀, T, Dc, U, Π) = signal(r,R,Cₛ,U)/noise(r,R,Cₛ,C₀,T,Dc,Π)
+@inline snr(r, R, Cₛ, C₀, T, Dc, U, Π) = signal(r, R, Cₛ, U) / noise(r, R, Cₛ, C₀, T, Dc, Π)
+
+@inline snrFD(r, R, Cₛ, C₀, T, Dc, U, Π) = signalFD(r, R, Cₛ, C₀, U, T) / noise(r, R, Cₛ, C₀, T, Dc, Π)
